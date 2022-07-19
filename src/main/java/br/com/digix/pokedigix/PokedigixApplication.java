47ff4aac9385f4ae315292efe1932b244375f2fb
Higor Nucci <higornucci@gmail.com>
2022-07-19 15:33:41 -0400

    Iniciando o projeto pokedigix

---
+++ src/main/java/br/com/digix/pokedigix/PokedigixApplication.java
@@ -0,0 +1,13 @@
+package br.com.digix.pokedigix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedigixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedigixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digix/pokedigix/PokedigixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digix.pokedigix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedigixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
