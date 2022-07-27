5fdc4065c1556ab04d1ac72043d6283440b5279a
Higor Nucci <higornucci@gmail.com>
2022-07-27 14:26:35 -0400

    Mapeamento de superclass personagem

---
+++ src/main/java/br/com/digix/pokedigix/personagem/Endereco.java
@@ -0,0 +1,38 @@
+package br.com.digix.pokedigix.personagem;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String regiao;
+    private String cidade;
+
+    public Endereco(String regiao, String cidade) {
+        this.regiao = regiao;
+        this.cidade = cidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }   
+    
+}
---
+++ src/main/java/br/com/digix/pokedigix/personagem/Treinador.java
@@ -0,0 +1,57 @@
+package br.com.digix.pokedigix.personagem;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import br.com.digix.pokedigix.pokemon.Pokemon;
+
+@Entity
+public class Treinador extends Personagem {
+
+    @Column(nullable = false)
+    private int dinheiro;
+    @Column(nullable = false)
+    private int nivel;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = Insignia.class)
+    @Column(name = "insignia")
+    private Collection<Insignia> insignias;
+
+    public Treinador(String nome, Endereco endereco, 
+                    Pokemon primeiroPokemon) {
+        super(nome, endereco);
+        this.capturar(primeiroPokemon);
+        this.dinheiro = 3000;
+        this.nivel = 1;
+        this.insignias = new ArrayList<>();
+    }
+
+    public void capturar(Pokemon pokemon) {
+        super.pokemons.add(pokemon);
+    }
+
+    public void receber(Insignia insignia) {
+        this.insignias.add(insignia);
+    }
+
+    public int getDinheiro() {
+        return dinheiro;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public Collection<Insignia> getInsignias() {
+        return insignias;
+    }
+    
+}
