c4694ad88b586412d23274c3a3d76239d5742d41
Higor Nucci <higornucci@gmail.com>
2022-08-17 14:30:04 -0400

    Adicionando o Swagger e documentando a API

--- src/main/java/br/com/digix/pokedigix/tipo/TipoController.java
+++ src/main/java/br/com/digix/pokedigix/tipo/TipoController.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RestController
 @RequestMapping(path = { "/api/v1/tipos" }, produces = { "application/json" })
 public class TipoController {
@@ -23,15 +28,22 @@ public class TipoController {
     @Autowired
     private TipoRepository tipoRepository;
 
+    @Operation(summary = "Criar um novo tipo que pode ser usado para Pokemons ou Ataques")
+    @ApiResponse(responseCode = "201")
     @PostMapping(consumes = { "application/json" })
-    public TipoResponseDTO criarTipo(@RequestBody TipoRequestDTO novoTipo) {
+    public ResponseEntity<TipoResponseDTO> criarTipo(@RequestBody TipoRequestDTO novoTipo) {
         Tipo tipo = new Tipo(novoTipo.getNome());
         tipoRepository.save(tipo);
-        return new TipoResponseDTO(tipo.getId(), tipo.getNome());
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new TipoResponseDTO(tipo.getId(), tipo.getNome()));
     }
 
+    @Operation(summary = "Buscar todos os tipos sem ordem")
+    @ApiResponse(
+        responseCode = "200", 
+        description = "Lista de tipos cadastrados")
     @GetMapping
-    public Collection<TipoResponseDTO> buscarTodos(
+    public ResponseEntity<Collection<TipoResponseDTO>> buscarTodos(
             @RequestParam(required = false, name = "termo") String nome) {
         Iterable<Tipo> tipos;
         if (nome != null) {
@@ -43,33 +55,42 @@ public class TipoController {
         for (Tipo tipo : tipos) {
             tiposRetornados.add(new TipoResponseDTO(tipo.getId(), tipo.getNome()));
         }
-        return tiposRetornados;
+        return ResponseEntity.ok(tiposRetornados);
     }
 
+    @Operation(summary = "Buscar um Tipo pelo seu id")
+    @ApiResponse(responseCode = "200")
     @GetMapping(path = "/{id}")
-    public TipoResponseDTO buscarPorId(@PathVariable Long id) {
+    public ResponseEntity<TipoResponseDTO> buscarPorId(@PathVariable Long id) {
         Tipo tipo = tipoRepository.findById(id).get();
-        return new TipoResponseDTO(tipo.getId(), tipo.getNome());
+        return ResponseEntity.ok(new TipoResponseDTO(tipo.getId(), tipo.getNome()));
     }
 
+    @Operation(summary = "Deletar um Tipo pelo seu id")
+    @ApiResponse(responseCode = "204")
     @DeleteMapping(path = "/{id}")
-    public void removerTipoPorId(@PathVariable Long id) {
+    public ResponseEntity<?> removerTipoPorId(@PathVariable Long id) {
         tipoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Deletar um Tipo pelo seu nome parcial ou completo")
+    @ApiResponse(responseCode = "204")
     @DeleteMapping
     @Transactional
-    public void removerTipoPorNome(@RequestParam(required = true) String termo) {
+    public ResponseEntity<?> removerTipoPorNome(@RequestParam(required = true) String termo) {
         tipoRepository.deleteByNomeContaining(termo);
+        return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Atualizar o Tipo")
+    @ApiResponse(responseCode = "200", description = "Retorna os dados atualizados")
     @PutMapping(path = "/{id}", consumes = "application/json")
-    public TipoResponseDTO alterarTipo(@RequestBody TipoRequestDTO tipoRequestDTO,
+    public ResponseEntity<TipoResponseDTO> alterarTipo(@RequestBody TipoRequestDTO tipoRequestDTO,
             @PathVariable Long id) {
         Tipo tipoParaAlterar = tipoRepository.findById(id).get();
         tipoParaAlterar.setNome(tipoRequestDTO.getNome());
         tipoRepository.save(tipoParaAlterar);
-        return new TipoResponseDTO(tipoParaAlterar.getId(), tipoParaAlterar.getNome());
+        return ResponseEntity.ok(new TipoResponseDTO(tipoParaAlterar.getId(), tipoParaAlterar.getNome()));
     }
-
 }
