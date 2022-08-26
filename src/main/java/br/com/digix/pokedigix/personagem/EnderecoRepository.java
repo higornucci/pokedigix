0957ff932d6f499557bb3fa435f4843a2bd502d4
Lennon Martins <lennonmcarlos@gmail.com>
2022-08-26 14:09:30 -0400

    tarefa_19 - buscar endereco por cidade

--- src/main/java/br/com/digix/pokedigix/personagem/EnderecoRepository.java
+++ src/main/java/br/com/digix/pokedigix/personagem/EnderecoRepository.java
@@ -1,7 +1,11 @@
 package br.com.digix.pokedigix.personagem;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
     
+    Collection <Endereco> findByCidadeContaining (String cidade);
+
 }
