3f018031d8e9670b236ebb6315fe7211dd2572f8
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2022-09-15 15:26:18 -0400

    Inclusao de Lombok

--- src/main/java/br/com/digix/pokedigix/tipo/TipoRequestDTO.java
+++ src/main/java/br/com/digix/pokedigix/tipo/TipoRequestDTO.java
@@ -1,21 +1,16 @@
 package br.com.digix.pokedigix.tipo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TipoRequestDTO {
 
     private String nome;
-
-    protected TipoRequestDTO(){}
-    
-    public TipoRequestDTO(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
 }
