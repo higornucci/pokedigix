f4e692686e1b2db783ff328046379ec634f7b795
EnzoGabrielBarbosaFranco <enzoddos7@gmail.com>
2022-09-15 15:18:14 -0400

    ATT LomBok AtaqueDTO, Response e Request

--- src/main/java/br/com/digix/pokedigix/pokemon/Pokemon.java
+++ src/main/java/br/com/digix/pokedigix/pokemon/Pokemon.java
@@ -16,7 +16,6 @@ import br.com.digix.pokedigix.ataque.Ataque;
 import br.com.digix.pokedigix.personagem.Treinador;
 import br.com.digix.pokedigix.tipo.Tipo;
 import br.com.digix.pokedigix.utils.EntidadeBase;
-import lombok.Builder;
 
 @Entity
 public class Pokemon extends EntidadeBase {
