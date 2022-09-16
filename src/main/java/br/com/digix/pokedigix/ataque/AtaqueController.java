d4c2ed47a6c2b28634e7755ecb23d2ce5cbc2010
EnzoGabrielBarbosaFranco <enzoddos7@gmail.com>
2022-09-16 14:20:54 -0400

    Ataque Service e Mudança no AtaqueController - Enzão e KaioKen

--- src/main/java/br/com/digix/pokedigix/ataque/AtaqueController.java
+++ src/main/java/br/com/digix/pokedigix/ataque/AtaqueController.java
@@ -1,6 +1,7 @@
 package br.com.digix.pokedigix.ataque;
 
-import java.util.Optional;
+
+import javax.naming.NameNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,9 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.webjars.NotFoundException;
 
-import br.com.digix.pokedigix.mappers.AtaqueMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
@@ -24,34 +23,21 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 public class AtaqueController {
 
   @Autowired
-  private AtaqueRepository ataqueRepository;
-
-  @Autowired
-  private AtaqueMapper ataqueMapper;
+  private AtaqueService ataqueService;
 
   @Operation(summary = "Buscar um ataque pelo seu id")
   @ApiResponse(responseCode = "200", description = "Retorna os dados do ataque solicitado")
   @GetMapping(path = "/{id}")
-  public ResponseEntity<AtaqueResponseDTO> buscarPorId(@PathVariable Long id) {
-    Optional<Ataque> ataqueOptional = ataqueRepository.findById(id);
-    if (ataqueOptional.isEmpty()) {
-      throw new NotFoundException(null);
-    }
-    Ataque ataque = ataqueOptional.get();
-    return ResponseEntity.ok(ataqueMapper.ataqueParaAtaqueResponseDTO(ataque));
+  public ResponseEntity<AtaqueResponseDTO> buscarPorId(@PathVariable Long id) throws NameNotFoundException {
+    return ResponseEntity.ok(ataqueService.buscarPorId(id));
   }
 
   @Operation(summary = "Criar um novo Ataque que pode ser usado para Pokemons")
   @ApiResponse(responseCode = "201")
   @PostMapping(consumes = { "application/json" })
-  public ResponseEntity<AtaqueResponseDTO> criar(
-      @RequestBody AtaqueRequestDTO novoAtaque)
-      throws AcuraciaInvalidaException, ForcaInvalidaParaCategoriaException, TipoInvalidoParaCategoriaException {
-    Ataque ataque = ataqueMapper.ataqueRequestParaAtaque(novoAtaque);
-    ataqueRepository.save(ataque);
-    return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(ataqueMapper.ataqueParaAtaqueResponseDTO(ataque));
+    public ResponseEntity<AtaqueResponseDTO> criarAtaque(@RequestBody AtaqueRequestDTO novoAtaque) throws AcuraciaInvalidaException, ForcaInvalidaParaCategoriaException, TipoInvalidoParaCategoriaException {
+      return ResponseEntity.status(HttpStatus.CREATED)
+          .body(ataqueService.criar(novoAtaque));
   }
 
   @Operation(summary = "Atualizar um Ataque")
@@ -60,28 +46,14 @@ public class AtaqueController {
 
   public ResponseEntity<AtaqueResponseDTO> atualizarTreinador(@RequestBody AtaqueRequestDTO ataqueRequestDTO,
       @PathVariable Long id) {
-    Optional<Ataque> ataqueOptional = ataqueRepository.findById(id);
-    if (ataqueOptional.isEmpty()) {
-      throw new NotFoundException(null);
-    }
-    Ataque ataque = ataqueOptional.get();
-    ataque.setNome(ataqueRequestDTO.getNome());
-    ataque.setAcuracia(ataqueRequestDTO.getAcuracia());
-    ataque.setCategoria(ataqueRequestDTO.getCategoria());
-    ataque.setDescricao(ataqueRequestDTO.getDescricao());
-    ataque.setForca(ataqueRequestDTO.getForca());
-    ataque.setPontosDePoder(ataqueRequestDTO.getPontosDePoder());
-    ataqueRepository.save(ataque);
-
-    return ResponseEntity.ok(ataqueMapper.ataqueParaAtaqueResponseDTO(ataque));
-
+        return ResponseEntity.ok(ataqueService.alterar(ataqueRequestDTO, id));
   }
 
   @Operation(summary = "Deletar um Ataque pelo seu id")
   @ApiResponse(responseCode = "204")
   @DeleteMapping(path = "/{id}")
   public ResponseEntity<Void> removerAtaqueId(@PathVariable Long id) {
-    ataqueRepository.deleteById(id);
+    ataqueService.removerPorId(id);
     return ResponseEntity.noContent().build();
   }
 }
