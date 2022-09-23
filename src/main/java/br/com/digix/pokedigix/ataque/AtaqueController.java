15d43e4a8f665f72e11021a94822b60fb35ea08f
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2022-09-23 16:14:59 -0400

    adicionado CrossOrigin

--- src/main/java/br/com/digix/pokedigix/ataque/AtaqueController.java
+++ src/main/java/br/com/digix/pokedigix/ataque/AtaqueController.java
@@ -6,6 +6,7 @@ import javax.naming.NameNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = { "/api/v1/ataques" }, produces = { "application/json" })
 public class AtaqueController {
 
