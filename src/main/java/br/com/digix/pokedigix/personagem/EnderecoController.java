6515759573d3ed72142b095cc14c6e2815f4f67a
Kaio Rodrigues Saravy <kaiorodriguessaravy@gmail.com>
2022-10-06 12:31:08 -0400

    att

--- src/main/java/br/com/digix/pokedigix/personagem/EnderecoController.java
+++ src/main/java/br/com/digix/pokedigix/personagem/EnderecoController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = { "/api/v1/enderecos" }, produces = { "application/json" })
 public class EnderecoController {
    @Autowired
