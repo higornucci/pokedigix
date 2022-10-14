883cbcacf823f909e8c68c25cf1243b4ba983a42
Lennon Martins <lennonmcarlos@gmail.com>
2022-10-14 13:12:28 -0400

    PokemonService

--- src/main/java/br/com/digix/pokedigix/pokemon/PokemonRepository.java
+++ src/main/java/br/com/digix/pokedigix/pokemon/PokemonRepository.java
@@ -2,14 +2,18 @@ package br.com.digix.pokedigix.pokemon;
 
 import java.util.Collection;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PokemonRepository extends CrudRepository<Pokemon, Long> {
+public interface PokemonRepository extends PagingAndSortingRepository<Pokemon, Long> {
 	Long deleteByNomeContaining(String nome);
 
 	@Query("SELECT p FROM Pokemon p JOIN p.tipos t WHERE t.id = :tipoId") // jpql
 	Collection<Pokemon> buscarPorTipo(Long tipoId);
 
 	Collection<Pokemon> findByNomeContaining(String nome);
+	
+	Page<Pokemon> findByNomeContaining(String nome, Pageable pageable);
 }
