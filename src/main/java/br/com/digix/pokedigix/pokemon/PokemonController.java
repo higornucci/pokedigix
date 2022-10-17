8b9c8b7d8e47c286b0b29b37eb88f680a69a6b29
Higor Nucci <higornucci@gmail.com>
2022-10-17 13:30:11 -0400

    Retornando o total de paginas ao buscar pokemons

--- src/main/java/br/com/digix/pokedigix/pokemon/PokemonController.java
+++ src/main/java/br/com/digix/pokedigix/pokemon/PokemonController.java
@@ -77,9 +77,9 @@ public class PokemonController {
   @Operation(summary = "Retorna uma lista de Pokémons ordenados e filtrados baseado nos parametros da busca")
   @ApiResponse(responseCode = "200", description = "Lista de Pokemons encontrados")
   @GetMapping
-  public ResponseEntity<Collection<PokemonResponseDTO>> buscarPeloNome(
+  public ResponseEntity<PokemonResponsePageDTO> buscarPeloNome(
       @RequestParam(required = false, name = "pagina", defaultValue = "0") int pagina,
-      @RequestParam(required = false, name = "tamanho", defaultValue = "5") int tamanho,
+      @RequestParam(required = false, name = "tamanho", defaultValue = "4") int tamanho,
       @RequestParam(required = false, name = "campoOrdenacao", defaultValue = "numeroPokedex") String campoOrdenacao,
       @RequestParam(required = false, name = "direcao", defaultValue = "ASC") String direcao,
       @RequestParam(required = false, name = "termo") String nome
