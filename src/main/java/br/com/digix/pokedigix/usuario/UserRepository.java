77d2b83b950d2f3fc8eb2ec8f3678382d249419a
EnzoGabrielBarbosaFranco <enzoddos7@gmail.com>
2022-11-16 14:28:44 -0400

    Att

--- src/main/java/br/com/digix/pokedigix/usuario/UserRepository.java
+++ src/main/java/br/com/digix/pokedigix/usuario/UserRepository.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<Usuario, Long> {
-    Optional<Usuario> findByUserName(String username);
+    Optional<Usuario> findByUsername(String username);
 
-    Boolean existsByUserName(String username);
+    Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
 }
