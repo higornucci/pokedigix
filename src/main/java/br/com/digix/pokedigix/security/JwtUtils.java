9c22fff27011915b7171bea145627b4a21268fc5
Igor Françozo <igorfrancozo9@gmail.com>
2022-12-08 13:07:58 -0400

    Pull Dev

---
+++ src/main/java/br/com/digix/pokedigix/security/JwtUtils.java
@@ -0,0 +1,57 @@
+package br.com.digix.pokedigix.security;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+
+import br.com.digix.pokedigix.usuario.UserDetailsImpl;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtUtils {
+	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+	@Value("${pokedigix.app.jwtSecret}")
+	private String jwtSecret;
+
+	@Value("${pokedigix.app.jwtExpirationsMs}")
+	private int jwtExpirationsMs;
+
+	public String generateJwtToken(Authentication authentication) {
+		UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+		return Jwts.builder().setSubject((userPrincipal.getUsername())).setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime() + jwtExpirationsMs))
+				.signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), SignatureAlgorithm.HS512).compact();
+	}
+
+	public String getUserNameFromJwtToken(String token) {
+		return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+				.build().parseClaimsJws(token).getBody().getSubject();
+	}
+
+	public boolean validateJwtToken(String authToken) {
+		try {
+			Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+					.build().parseClaimsJwt(authToken);
+			return true;
+		} catch (SecurityException e) {
+			logger.error("Invalid JWT signature: {}", e.getMessage());
+		} catch (MalformedJwtException e) {
+			logger.error("Invalid JWT token: {}", e.getMessage());
+		} catch (ExpiredJwtException e) {
+			logger.error("Invalid JWT is expired: {}", e.getMessage());
+		} catch (UnsupportedJwtException e) {
+			logger.error("JWT token is unsupported: {}", e.getMessage());
+		} catch (IllegalArgumentException e) {
+			logger.error("JWT claims string is empty: {}", e.getMessage());
+		}
+		return false;
+	}
+}
---
+++ src/main/java/br/com/digix/pokedigix/usuario/ERole.java
@@ -0,0 +1,7 @@
+package br.com.digix.pokedigix.usuario;
+
+public enum ERole {
+	ROLE_TREINADOR,
+	ROLE_LIDER,
+	ROLE_ADMIN
+}
